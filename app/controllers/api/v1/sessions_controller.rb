ddddce9f7a8a8b50edeed60ee885c0896f4201ef
Juan Manuel Gomez Duarte <juan.gomez@minosit.com>
2018-08-23 11:32:58 -0500

    Ultimas actualizaciones de auhtenticacion por api

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,8 +3,8 @@ class Api::V1::SessionsController < Devise::SessionsController
     skip_before_action :verify_authenticity_token
     
     def create
-      user  = User.find_for_database_authentication(email: params[:user][:email])
-      if user && user.valid_password?(params[:user][:password])
+      user = User.find_for_database_authentication({email: params[:email]}) if defined? params[:email]
+      if user && user.valid_password?(params[:password])
         auth_token = jwt_token(user)
         render json: {auth_token: auth_token}
       else
